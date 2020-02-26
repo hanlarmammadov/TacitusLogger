b2a9b595a2e25eaa4eaeaffb3bb56d7342117701
Khanlar Mammadov <khanlar@mammadov.pro>
2020-02-26 21:41:10 +0400

    Ignored files in TacitusLogger/Destinations/Debug folder were added

---
+++ src/TacitusLogger/Destinations/Debug/DebugConsoleFacade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TacitusLogger.Destinations.Debug
+{
+    /// <summary>
+    /// A facade for System.Diagnostics.Debug.
+    /// This class is internal.
+    /// </summary>
+    internal class DebugConsoleFacade : IOutputDeviceFacade
+    {
+        /// <summary>
+        /// Writes the <paramref name="text"/> to the <c>System.Diagnostics.Debug.</c>
+        /// </summary>
+        /// <param name="text"></param>
+        public void WriteLine(string text)
+        {
+            System.Diagnostics.Debug.WriteLine(text);
+        }
+        /// <summary>
+        /// Asynchronously writes the <paramref name="text"/> to the <c>System.Diagnostics.Debug.</c>
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        public Task WriteLineAsync(string text, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return Task.FromCanceled(cancellationToken);
+            if (text == null)
+                throw new ArgumentNullException("text");
+            System.Diagnostics.Debug.WriteLine(text);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/TacitusLogger/Destinations/Debug/DebugDestination.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using TacitusLogger.Serializers; 
+
+namespace TacitusLogger.Destinations.Debug
+{
+    /// <summary>
+    /// Destination that writes log model to System.Diagnostics.Debug.
+    /// </summary>
+    public class DebugDestination : NetDiagnosticsLogDestinationBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the TacitusLogger.Destinations.DebugDestination class using 
+        /// log serializer.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">If <paramref name="logSerializer"/> is null.</exception>
+        /// <param name="logSerializer">Log serializer.</param>
+        public DebugDestination(ILogSerializer logSerializer)
+            : base(logSerializer, new DebugConsoleFacade())
+        {
+
+        } 
+        /// <summary>
+        /// Initializes a new instance of the TacitusLogger.Destinations.DebugDestination class using 
+        /// log string template.
+        /// </summary>        
+        /// <exception cref="ArgumentNullException">If <paramref name="logStringTemplate"/> is null.</exception>
+        /// /// <param name="logStringTemplate">Log string template.</param>
+        public DebugDestination(string logStringTemplate)
+             : this(new SimpleTemplateLogSerializer(logStringTemplate))
+        {
+
+        }
+        /// <summary>
+        /// Initializes a new instance of the TacitusLogger.Destinations.DebugDestination class using 
+        /// log string factory method.
+        /// </summary>    
+        /// <exception cref="ArgumentNullException">If <paramref name="logStringFactoryMethod"/> is null.</exception>
+        /// <param name="logStringFactoryMethod">Log string factory method</param>
+        public DebugDestination(LogModelFunc<string> logStringFactoryMethod)
+             : this(new GeneratorFunctionLogSerializer(logStringFactoryMethod))
+        {
+
+        }
+        /// <summary>
+        /// Initializes a new instance of the TacitusLogger.Destinations.DebugDestination class using 
+        /// default log serializer.
+        /// </summary>
+        /// 
+        public DebugDestination()
+             : this(new SimpleTemplateLogSerializer())
+        {
+
+        } 
+    }
+}

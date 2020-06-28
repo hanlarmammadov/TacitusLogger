a856695cdabb621af1aa002102fa46e8f69ba756
Khanlar Mammadov <khanlar@mammadov.pro>
2020-06-28 20:02:26 +0400

    Fixing thread safety issue in mutable settings.

--- src/TacitusLogger/MutableSetting.cs
+++ src/TacitusLogger/MutableSetting.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using System.Text;
 
 namespace TacitusLogger
@@ -6,14 +6,22 @@ namespace TacitusLogger
     public class MutableSetting<TValue> : Setting<TValue>
     {
         private TValue _value;
+        private Object _valueLocker = new Object();
 
         public MutableSetting(TValue initialValue = default(TValue))
         {
-            SetValue(initialValue);
+            _value = initialValue;
         }
 
-        public override TValue Value => _value;
-        
+        public override TValue Value
+        {
+            get
+            {
+                lock (_valueLocker)
+                    return _value;
+            }
+        }
+
         public static implicit operator MutableSetting<TValue>(TValue value)
         {
             var obj = new MutableSetting<TValue>();
@@ -23,10 +31,11 @@ namespace TacitusLogger
 
         public void SetValue(TValue value)
         {
-            _value = value;
+            lock (_valueLocker)
+                _value = value;
         }
         public override string ToString()
-        { 
+        {
             return new StringBuilder()
                .Append($"MutableSetting [Value = {_value}]")
                .ToString();
---
+++ src/TacitusLogger/StoredReadonlySettingBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TacitusLogger.Components.Time;
+
+namespace TacitusLogger
+{
+    public abstract class StoredReadonlySettingBase<TValue> : Setting<TValue>
+    {
+        private readonly long _valueReloadIntervalTicks;
+        private ITimeProvider _timeProvider;
+        private long _lastUpdateTicks;
+        private TValue _value;
+        private Object _valueLocker = new Object();
+
+        public StoredReadonlySettingBase(int fetchValueIntervalMilliseconds)
+        {
+            _valueReloadIntervalTicks = fetchValueIntervalMilliseconds * 10_000;
+            _timeProvider = new SystemTimeProvider();
+        }
+
+        public override TValue Value
+        {
+            get
+            {
+                if (_timeProvider.GetLocalTime().Ticks - _lastUpdateTicks > _valueReloadIntervalTicks)
+                    lock (_valueLocker)
+                        if (_timeProvider.GetLocalTime().Ticks - _lastUpdateTicks > _valueReloadIntervalTicks)
+                        {
+                            _value = LoadValue();
+                            _lastUpdateTicks = _timeProvider.GetLocalTime().Ticks;
+                        }
+                return _value;
+            }
+        }
+        internal void ResetTimeProvider(ITimeProvider timeProvider)
+        {
+            lock (_valueLocker)
+                _timeProvider = timeProvider ?? throw new ArgumentNullException("timeProvider");
+        }
+        protected abstract TValue LoadValue();
+    }
+}
